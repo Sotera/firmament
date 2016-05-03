4587ac77a27cf5c6503fae109db022a04851c7fd
John Reeme <jreeme@localhost.localdomain>
2016-05-03 14:38:17 -0400

    Uncomment important lines in cmake install script

--- docker/centos/7.0/_install_cmake-3.sh
+++ docker/centos/7.0/_install_cmake-3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#sudo yum install -y wget
-#wget https://cmake.org/files/v3.5/cmake-3.5.2-Linux-x86_64.tar.gz
-#tar xvf cmake-3.5.2-Linux-x86_64.tar.gz
+sudo yum install -y wget
+wget https://cmake.org/files/v3.5/cmake-3.5.2-Linux-x86_64.tar.gz
+tar xvf cmake-3.5.2-Linux-x86_64.tar.gz
 cd "cmake-3.5.2-Linux-x86_64/bin"
 DIR=$(pwd)
 cd -
