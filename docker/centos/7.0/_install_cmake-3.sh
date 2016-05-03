14e9eb2cd9a3c7a1cad8f5863f49d4ba50fcf151
John Reeme <jreeme@localhost.localdomain>
2016-05-03 14:36:02 -0400

    Extract cmake TAR file instead of using cmake shell install script

--- docker/centos/7.0/_install_cmake-3.sh
+++ docker/centos/7.0/_install_cmake-3.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-sudo yum install -y wget
-sudo wget https://cmake.org/files/v3.5/cmake-3.5.2-Linux-x86_64.sh
-sudo chmod 755 cmake-3.5.2-Linux-x86_64.sh
-./cmake-3.5.2-Linux-x86_64.sh
+#sudo yum install -y wget
+#wget https://cmake.org/files/v3.5/cmake-3.5.2-Linux-x86_64.tar.gz
+#tar xvf cmake-3.5.2-Linux-x86_64.tar.gz
+cd "cmake-3.5.2-Linux-x86_64/bin"
+DIR=$(pwd)
+cd -
+sudo ln -s "$DIR"/* /usr/local/bin/
