8383f2ac55125e2444a2d3a4f400106758270f82
John Reeme <john.reeme@soteradefense.com>
2016-05-16 15:36:49 -0500

    Tune up global install of firmament module.

--- _install_node.sh
+++ _install_node.sh
@@ -8,9 +8,10 @@ if ! type "curl" >/dev/null;
         exit 1
 fi
 
-sudo sh -c "curl https://nodejs.org/dist/v5.9.1/node-v5.9.1-linux-x64.tar.xz | tar -xJ --directory /var/local"
+sudo sh -c "curl https://nodejs.org/dist/v5.9.1/node-v5.9.1-linux-x64.tar.xz | tar -C /usr/local --strip-components 1 -xJ"
+#sudo sh -c "curl https://nodejs.org/dist/v5.9.1/node-v5.9.1-linux-x64.tar.xz | tar -xJ --directory /var/local"
 #sudo sh -c "curl https://nodejs.org/dist/v6.1.0/node-v6.1.0-linux-x64.tar.xz | tar -xJ --directory /var/local"
 #sudo ln -s /var/local/node-v6.1.0-linux-x64/bin/* /usr/local/bin/
-sudo ln -s /var/local/node-v5.9.1-linux-x64/bin/node /usr/local/bin/
-sudo ln -s /var/local/node-v5.9.1-linux-x64/bin/npm /usr/local/bin/
-sudo npm i -g firmament
+#sudo ln -s /var/local/node-v5.9.1-linux-x64/bin/node /usr/local/bin/
+#sudo ln -s /var/local/node-v5.9.1-linux-x64/bin/npm /usr/local/bin/
+#sudo npm i -g firmament
