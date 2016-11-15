0f5f68939876997d924034b47ecd789a1d4aeef6
John Reeme <john.reeme@soteradefense.com>
2016-11-15 15:56:14 -0600

    Pushing to NPM v.0.2.26

--- js/firmament.js
+++ js/firmament.js
@@ -1,16 +1,27 @@
 #!/usr/bin/env node
 "use strict";
 require('reflect-metadata');
-const firmament_docker_1 = require('firmament-docker');
-let commandLine = firmament_docker_1.kernel.get('CommandLine');
+const firmament_yargs_1 = require('firmament-yargs');
+const _ = require('lodash');
+const commandLine = firmament_yargs_1.kernel.get('CommandLine');
+const spawn = firmament_yargs_1.kernel.get('Spawn');
 commandLine.init({
     version: () => {
         return require('../package.json').version;
     }
 });
-let dockerCommand = firmament_docker_1.kernel.get('DockerCommand');
-commandLine.addCommand(dockerCommand);
-let makeCommand = firmament_docker_1.kernel.get('MakeCommand');
-commandLine.addCommand(makeCommand);
-commandLine.exec();
+function processNpmInfo(npmInfo) {
+    let firmamentModules = Object.getOwnPropertyNames(npmInfo.dependencies).filter(key => {
+        return _.startsWith(key, 'firmament-');
+    });
+    firmamentModules.forEach(moduleName => {
+        let kernel = require(moduleName).kernel;
+        let commands = kernel.getAll('Command');
+        commands.forEach(command => {
+            commandLine.addCommand(command);
+        });
+    });
+    commandLine.exec();
+}
+processNpmInfo(require('../package.json'));
 //# sourceMappingURL=firmament.js.map
--- ts/firmament.ts
+++ ts/firmament.ts
@@ -1,18 +1,48 @@
 #!/usr/bin/env node
 import 'reflect-metadata';
-import {kernel as firmamentDockerKernel} from 'firmament-docker';
-import {Command, CommandLine} from 'firmament-yargs';
-let commandLine = firmamentDockerKernel.get<CommandLine>('CommandLine');
+//import {kernel as firmamentDockerKernel} from 'firmament-docker';
+import {Command, CommandLine, Spawn, kernel} from 'firmament-yargs';
+//let commandLine = firmamentDockerKernel.get<CommandLine>('CommandLine');
+import * as _ from 'lodash';
+import {interfaces as container_interfaces} from "inversify";
+const commandLine = kernel.get<CommandLine>('CommandLine');
+const spawn = kernel.get<Spawn>('Spawn');
 commandLine.init(
   {
-    version: ()=> {
+    version: () => {
       return require('../package.json').version;
     }
   }
 );
-let dockerCommand = firmamentDockerKernel.get<Command>('DockerCommand');
-commandLine.addCommand(dockerCommand);
-let makeCommand = firmamentDockerKernel.get<Command>('MakeCommand');
-commandLine.addCommand(makeCommand);
-commandLine.exec();
+interface NpmInfo {
+  dependencies: any
+}
+function processNpmInfo(npmInfo:NpmInfo){
+  let firmamentModules = Object.getOwnPropertyNames(npmInfo.dependencies).filter(key => {
+    return _.startsWith(key, 'firmament-');
+  });
+  firmamentModules.forEach(moduleName => {
+    let kernel: container_interfaces.Container = require(moduleName).kernel;
+    let commands = kernel.getAll<Command>('Command');
+    commands.forEach(command => {
+      commandLine.addCommand(command);
+    });
+  });
+  commandLine.exec();
+}
+processNpmInfo(<NpmInfo>require('../package.json'));
+/*spawn.spawnShellCommandAsync(
+  ['npm', 'ls', '--json'],
+  {
+    stdio: 'pipe',
+    cwd: require('path').resolve(__dirname, '..')
+  },
+  (err: Error, result: string) => {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+    let npmInfo: NpmInfo = JSON.parse(result);
+    processNpmInfo(npmInfo);
+  });*/
 
