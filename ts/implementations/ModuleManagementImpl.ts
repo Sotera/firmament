d3ac893413ddf1cac30dc3db9794764f048c94a8
John <john.reeme@gmail.com>
2018-11-08 14:59:09 -0600

    v.1.3.14 -> NPM & Rework npm call

--- ts/implementations/ModuleManagementImpl.ts
+++ ts/implementations/ModuleManagementImpl.ts
@@ -4,7 +4,6 @@ import {CommandUtil, Spawn} from "firmament-yargs";
 import path = require('path');
 import fs = require('fs');
 import * as _ from 'lodash';
-const which = require('which');
 
 @injectable()
 export class ModuleManagementImpl implements ModuleManagement {
@@ -18,28 +17,29 @@ export class ModuleManagementImpl implements ModuleManagement {
 
   installModule(argv: any) {
     let me = this;
-    if (!argv.name) {
+    if(!argv.name) {
       me.commandUtil.processExit(1, `\nPlease provide a module name using the '--name <module_name>' switch\n`);
     }
-    if (!_.startsWith(argv.name, me.modulePrefix)) {
+    if(!_.startsWith(argv.name, me.modulePrefix)) {
       argv.name = `${me.modulePrefix}${argv.name}`;
       //me.commandUtil.processExit(1, `\nModule names must start with '${me.modulePrefix}'\n`);
     }
     let prefix = path.resolve(__dirname, '../..');
-    let cmd = ['npm', 'install', '--save', '--prefix', `${prefix}`, argv.name];
-    me.spawn.spawnShellCommandAsync(cmd, null,
+    //let cmd = ['npm', 'install', '--save', '--prefix', `${prefix}`, argv.name];
+    let cmd = ['npm', 'install', '--save', argv.name];
+    me.spawn.spawnShellCommandAsync(cmd, {cwd: prefix},
       (err: Error, result: string) => {
         me.commandUtil.stdoutWrite(result);
       },
       (err: Error) => {
-        if (err) {
+        if(err) {
           //Assume the error was permission related and retry under 'sudo'
           me.spawn.sudoSpawnAsync(cmd, null,
             (err: Error, result: string) => {
               me.commandUtil.stdoutWrite(result);
             },
             (err: Error) => {
-              if (err) {
+              if(err) {
                 me.commandUtil.processExitWithError(err);
               } else {
                 me.commandUtil.processExit(0, `Looks like module '${argv.name}' installed successfully!`);
